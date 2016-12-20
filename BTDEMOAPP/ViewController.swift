2e25a79b83012499fee7c5a5dbb5f35496f31900
rn10rd9 <RN10RD9@christians-macbook-pro.local>
2016-12-19 19:21:47 -0600

    Initial Commit

---
+++ BTDEMOAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BTDEMOAPP
+//
+//  Created by Christian Chabikuli on 2016-12-19.
+//  Copyright © 2016 Christian Chabikuli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
